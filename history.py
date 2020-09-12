12ef01d4eef1de798c6f39620d466a6c0bb313e8
Rasazna <klsrasazna@gmail.com>
2020-09-11 22:56:39 -0700

    final project

---
+++ history.py
@@ -0,0 +1,106 @@
+runfile('C:/Users/Test/.spyder-py3/temp.py', wdir='C:/Users/Test/.spyder-py3')
+pip install tensorflow
+pip install tensorflow --user
+import tensorflow
+
+## ---(Sun Aug 16 03:20:45 2020)---
+runfile('C:/Users/Test/.spyder-py3/temp.py', wdir='C:/Users/Test/.spyder-py3')
+conda install tensorflow
+import tensorflow
+
+## ---(Sun Aug 16 03:53:08 2020)---
+import tensorflow
+pip install tensorflow
+import tensorflow
+
+## ---(Sun Aug 16 03:55:10 2020)---
+import tensorflow
+python --version
+python3 --version
+Python3 --version
+pip install --upgrade tensorflow
+runfile('C:/Users/Test/.spyder-py3/temp.py', wdir='C:/Users/Test/.spyder-py3')
+
+## ---(Sun Aug 16 04:17:47 2020)---
+import tensorflow
+pip install absl-py
+import tensorflow
+pip install numpy
+import tensorflow
+pip install wrapt
+import tensorflow
+
+## ---(Sun Aug 16 04:35:57 2020)---
+import tensorflow
+
+## ---(Sun Aug 16 05:00:42 2020)---
+import tensorflow as tf
+runfile('C:/Users/Test/.spyder-py3/temp.py', wdir='C:/Users/Test/.spyder-py3')
+pip install opencv-python-headless
+runfile('C:/Users/Test/.spyder-py3/temp.py', wdir='C:/Users/Test/.spyder-py3')
+pip install matplotlib
+runfile('C:/Users/Test/.spyder-py3/temp.py', wdir='C:/Users/Test/.spyder-py3')
+import cv2     # for capturing videos
+import math   # for mathematical operations
+import matplotlib.pyplot as plt    # for plotting the images
+%matplotlib inline
+import pandas as pd
+from keras.preprocessing import image   # for preprocessing the images
+import numpy as np    # for mathematical operations
+from keras.utils import np_utils
+from skimage.transform import resize
+runfile('C:/Users/Test/.spyder-py3/temp.py', wdir='C:/Users/Test/.spyder-py3')
+import panda
+import pandas
+pip install pandas
+runfile('C:/Users/Test/.spyder-py3/temp.py', wdir='C:/Users/Test/.spyder-py3')
+import ft2font
+import tensorflow
+runfile('C:/Users/Test/.spyder-py3/temp.py', wdir='C:/Users/Test/.spyder-py3')
+import matplotlib.pyplot            
+
+## ---(Sun Aug 16 05:17:10 2020)---
+import matplotlib.pyplot
+
+## ---(Sun Aug 16 05:21:49 2020)---
+import tensorflow
+import matplotlib.pyplot
+runfile('C:/Users/Test/.spyder-py3/temp.py', wdir='C:/Users/Test/.spyder-py3')
+pip install keras
+runfile('C:/Users/Test/.spyder-py3/temp.py', wdir='C:/Users/Test/.spyder-py3')
+tensorflow _version_             
+import tensorflow as tf
+tf._version_
+tf.VERSION
+runfile('C:/Users/Test/.spyder-py3/temp.py', wdir='C:/Users/Test/.spyder-py3')
+tf.VERSION
+pip install --user --upgrade tensorflow
+pip install keras==2.3.0
+runfile('C:/Users/Test/.spyder-py3/temp.py', wdir='C:/Users/Test/.spyder-py3')
+
+## ---(Sun Aug 16 06:33:52 2020)---
+runfile('C:/Users/Test/.spyder-py3/temp.py', wdir='C:/Users/Test/.spyder-py3')
+import tensorflow
+
+## ---(Sun Aug 16 06:36:48 2020)---
+import tensorflow
+pip install tensorflow
+import tensorflow
+
+## ---(Sun Aug 16 06:39:45 2020)---
+import tensorflow
+
+## ---(Sun Aug 16 06:43:04 2020)---
+import tensorflow
+pip install tensorflow=2.0
+pip install tensorflow==2.0
+import tensorflow
+
+## ---(Sun Aug 16 06:46:48 2020)---
+import tensorflow
+runfile('C:/Users/Test/.spyder-py3/temp.py', wdir='C:/Users/Test/.spyder-py3')
+
+## ---(Fri Sep 11 22:29:45 2020)---
+runfile('C:/Users/Test/.spyder-py3/temp.py', wdir='C:/Users/Test/.spyder-py3')
+pip install Tensorflow
+import Tensorflow
